231009786f1c6e06aa9861eb7389940328d81b8e
Mariya <marbel@mail.ru>
2022-08-29 22:29:39 +0300

    Example005 RandomSum

---
+++ Example005_RandomSum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10);// Случайное число 0 1 2 3 4 5 6 7 8 9
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
