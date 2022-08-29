4d16b903862b88ee069821dc6f6ac61d67838f5b
Mariya <marbel@mail.ru>
2022-08-29 21:31:12 +0300

    Add Example002_HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Print name ");//Напечатать Введите имя
+string username = Console.ReadLine();// Cчитывание данных в переменную username.Strign - cтрока
+Console.WriteLine("Hello, ");
+Console.WriteLine(username);
