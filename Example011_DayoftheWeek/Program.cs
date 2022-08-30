2061b9203e995d9a1ae39f9f39957bb21f16baa0
Mariya <marbel@mail.ru>
2022-08-30 11:14:12 +0300

    Example_Convert number of the day into days of the week

--- Example011_DayoftheWeek/Program.cs
+++ Example011_DayoftheWeek/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Write("Input number of the day between 1 and 7: ");
 int n = Convert.ToInt32(Console.ReadLine());//Перевод строки в число
-while (n<1||n<7)
+while (n<1||n>7)
 {
-    Console.Write("Are you stupid? Please repeat. Print a number between 1 and 7");
-    n=Converter.ToInt32(Console.ReadLine());
+    Console.Write("Are you stupid? Please repeat. Print a number between 1 and 7: ");
+    n = Convert.ToInt32(Console.ReadLine());
 }
 if (n == 1)
     Console.WriteLine("Monday");
@@ -19,3 +19,5 @@ if (n == 6)
     Console.WriteLine("Saturday");
 if (n == 7)
     Console.WriteLine("Sunday");
+
+
