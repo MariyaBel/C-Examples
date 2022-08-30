0290613f25e8e2e09a4f2a1daa884b36e98793eb
Mariya <marbel@mail.ru>
2022-08-30 10:57:06 +0300

    Add Example010_Check_if

---
+++ Example010_Check_if/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Input first number: ");
+int n = Convert.ToInt32(Console.ReadLine());//Перевод строки в число
+Console.Write("Input second number: ");
+int m = Convert.ToInt32(Console.ReadLine());//Перевод строки в число
+if (n * n == m)
+    Console.WriteLine("Yes!It's a square");
+else
+    Console.WriteLine("No.It's not a square");
