a532ba0a99e917727b75858d667d91811dd5e260
Mariya <marbel@mail.ru>
2022-08-30 11:29:51 +0300

    Add Example013 Input number Print last number

---
+++ Example013_Lastnumber/Program.cs
@@ -0,0 +1,4 @@
+﻿// программа принимает на вход трехзначное число и выдает его последнюю цифру
+Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n % 10);
