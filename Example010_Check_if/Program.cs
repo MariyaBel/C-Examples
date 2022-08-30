19f870f7a84fb689cd819a9541bb98250a6478e4
Mariya <marbel@mail.ru>
2022-08-30 11:25:23 +0300

    Example012_Input one number Print all

--- Example010_Check_if/Program.cs
+++ Example010_Check_if/Program.cs
@@ -1,21 +1,9 @@
-﻿Console.Write("Input number of the day between 1 and 7: ");
-int n = Convert.ToInt32(Console.ReadLine());//Перевод строки в число
-while (n<1||n<7)
-{
-    Console.Write("Are you stupid? Please repeat. Print a number between 1 and 7");
-    n=Converter.ToInt32(Console.ReadLine());
-}
-if (n == 1)
-    Console.WriteLine("Monday");
-if (n == 2)
-    Console.WriteLine("Tuesday");
-if (n == 3)
-    Console.WriteLine("Wednesday");
-if (n == 4)
-    Console.WriteLine("Thursday");
-if (n == 5)
-    Console.WriteLine("Friday");
-if (n == 6)
-    Console.WriteLine("Saturday");
-if (n == 7)
-    Console.WriteLine("Sunday");
+﻿Console.Write("Print first number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Print second number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m * m == n)
+    Console.WriteLine("yes, it's a square");
+else
+    Console.WriteLine("no, it's not a square");
+
---
+++ Example012_Onenumbertoall/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+//2 -> " -2, -1, 0, 1, 2"
+// task 5
+
+Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = (-1) * n; i <= n; i++)
+    Console.Write(i + " ");
+
+// task 5
+Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = (-1) * n;
+while (i <= n)
+{
+    Console.Write(i);
+    Console.Write(", ");
+    i++;
+}
