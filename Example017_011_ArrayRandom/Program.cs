9687a7095ea35518537222513c4cda90f1682057
Mariya <marbel@mail.ru>
2022-09-02 00:37:13 +0300

    Added Example017 Fuction FillArray and PrintArray

---
+++ Example017_011_ArrayRandom/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)// метод (функция) заполнения массива (коллекция)
+{
+    int length = collection.Length;// длина массива
+    int index = 0;
+    while (index < length)
+    {
+        collection[index]=new Random().Next(1,10);//заполнения элемента массива случаным числом
+        index++;//index=index+1
+    }
+}
+
+void PrintArray(int[] col)// метод (функция) по выводу массива в консоль
+{
+    int count = col.Length;// длина массива col
+    int position = 0;// index=0
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int[]array = new int[20];//создали массив в 20 чисел
+
+FillArray(array);//заполнение
+PrintArray(array);// вывод
