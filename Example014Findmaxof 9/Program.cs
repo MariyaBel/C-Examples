1a7d34b07eeae30d5ead154f4a555e800c36738b
Mariya <marbel@mail.ru>
2022-09-01 16:56:29 +0300

    Changed metod

--- Example014Findmaxof 9/Program.cs
+++ Example014Findmaxof 9/Program.cs
@@ -1,25 +1,27 @@
 ﻿int Max(int arg1, int arg2, int arg3)
 {
-int result = arg1;
-if (arg2>result) result =arg2;
-if (arg3>result) result =arg3;
-return result;
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
 }
 
-int a1=3;
-int a2=9;
-int a3=1490;
-int b1=12;
-int b2=5;
-int b3=10;
-int c1=90;
-int c2=6;
-int c3=7;
+int a1 = 3;
+int a2 = 9;
+int a3 = 1490;
+int b1 = 12;
+int b2 = 5;
+int b3 = 10;
+int c1 = 90;
+int c2 = 6457345;
+int c3 = 7;
 
-int max1=Max(a1,b1,c1);
-int max2=Max(a2,b2,c2);
-int max3=Max(a3,b3,c3);
-int max=Max(max1,max2,max3);
+//int max1=Max(a1,b1,c1);
+//int max2=Max(a2,b2,c2);
+//int max3=Max(a3,b3,c3);
+//int max=Max(max1,max2,max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 Console.Write(max);
 
 
