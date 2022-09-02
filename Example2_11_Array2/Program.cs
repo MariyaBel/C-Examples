2e840472ffb8a5090b1213e5047aefca3ade103d
Mariya <marbel@mail.ru>
2022-09-02 10:35:30 +0300

    Example2_11 Array

---
+++ Example2_11_Array2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное
+// число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+
+int GenerateRandomNumbers()// функция для вывода случайного числа от 100 до 999 включительно
+{
+    int n = new Random().Next(100, 1000);
+    return n;
+}
+
+void CompraiseNumbers()
+{
+    int n = GenerateRandomNumbers();
+    Console.WriteLine(n);
+    int a =n/100;// находим первую цифру (сотни)
+    int b = n%10;// находим третью цифру
+   
+    Console.WriteLine(a*10+b);
+    
+}
+
+CompraiseNumbers();
