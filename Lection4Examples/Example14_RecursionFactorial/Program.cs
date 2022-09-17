859a8b23771ac626094a8ce5f2e66feea9a06b41
Mariya <marbel@mail.ru>
2022-09-18 01:48:04 +0300

    Seminar6 Homework

---
+++ Lection4Examples/Example14_RecursionFactorial/Program.cs
@@ -0,0 +1,12 @@
+﻿double Factorial(int n)
+{
+    if (n == 1) return 1;// 1!=1 , 0!=1
+    else return n * Factorial(n-1);
+}
+
+Console.WriteLine(Factorial(5));//1*2*3=6
+
+for (int i=1;i<40;i++)
+{
+    Console.WriteLine(Factorial(i));
+}
---
+++ Seminar6Homework/Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("How many numbers will you input? ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number and press Enter: ");
+
+int[] arr = new int[n];
+int count=0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if (arr[i]>0) count++;
+}
+Console.WriteLine(count+" numbers > 0");
+
+
