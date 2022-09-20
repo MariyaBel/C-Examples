2a6d9693f0fe0fcc0f99fc4831b6600ca86cb975
Mariya <marbel@mail.ru>
2022-09-20 17:17:26 +0300

    Homework 7

---
+++ Seminar7Howework_Matrix/Task52/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+
+void InputMatrix(int[,] matrix)// ввод рандомного массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-100, 101);
+    }
+}
+
+void PrintMatrix(int[,] matrix)// вывод матрицы на экран
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + " \t");
+        Console.WriteLine();
+    }
+}
+
+void ReleaseJAverage(int[,] matrix)
+{
+    int sum = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.Write("sum="+sum+ " ");
+    }
+}
+
+Console.Write("Input number of rows: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+ReleaseJAverage(matrix);
+
